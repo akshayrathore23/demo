934fe1e9fb783bde5e6c99b104b2e3ab606fcb5f
akshayrathore23 <rathoreakshay673@gmail.com>
2024-09-18 18:55:33 +0530

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+function showsidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function closesidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
